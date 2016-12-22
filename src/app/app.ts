1be0c781b75ef11c1091b06aeeafa0a2e55dbeab
Bornwell Matembudze <matembudzeb@gmail.com>
2016-12-22 18:20:28 +0200

    Module 1 complete

---
+++ src/app/app.ts
@@ -0,0 +1,15 @@
+/**
+ * Created by bornie21 on 22/12/16.
+ */
+import {Component, Class} from '@angular/core';
+
+@Component({
+    selector: 'retain-app',
+    template: `
+<div>
+<h3>Yo World</h3>
+</div>
+`
+})
+
+export class App {};
