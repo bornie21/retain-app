e07d6262976f85f14c96996f3e4bc7e66d25e6ec
Bornwell Matembudze <matembudzeb@gmail.com>
2016-12-27 14:23:50 +0200

    Module 15 complete

---
+++ src/app/containers/auth.ts
@@ -0,0 +1,14 @@
+/**
+ * Created by bornie21 on 27/12/16.
+ */
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'auth-container',
+  template: `     <div class="auth-container">
+      <h1>Auth</h1>
+    </div>
+  `
+})
+
+export class Auth{}
